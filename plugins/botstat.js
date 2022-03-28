59895041f903f88488c07ac72bd390b054849fde
Fahzz <101871868+Fahzz@users.noreply.github.com>
2022-03-28 14:46:22 +0800

    Update botstat.js

--- plugins/botstat.js
+++ plugins/botstat.js
@@ -11,8 +11,8 @@ let handler = async (m, { conn }) => {
     let str = `
 ╭═════════════════
 ║╭─❉ [𝐈𝐍𝐅𝐎𝐁𝐎𝐓] ❉───
-║│➸ NameBot: LynXzy
-║│➸ Main Owner: LanXzy
+║│➸ NameBot: FahzBot
+║│➸ Main Owner: Fahzz
 ║│➸ Operated Since: Lupa gw
 ║│➸ Language Used: Nodejs (JS)
 ║│➸ Server: Heroku
