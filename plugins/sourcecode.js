334cd2a067548608b50f6b0fa0c788b413613d9b
Fahzz <101871868+Fahzz@users.noreply.github.com>
2022-03-28 14:06:57 +0800

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'Source code')).buffer(), `
-𝙎𝘾 𝙊𝙍𝙄 𝘽𝙔 𝙇𝙔𝙉𝙓𝙕𝙔 🔥
-github.com/LynnXzy/scbotz-v1
+*SC BY FAHZ* 🔥
+https://github.com/Fahzz/scbotz-v1
 `.trim(), '©LynXzy🥀', 'Makasih', 'Thanks')
 handler.help = ['sourcecode']
 handler.tags = ['info']
