e4a508720efe406ecc05df93b11adc375bc92e1e
SaboBootz <101971205+SaboBootz@users.noreply.github.com>
2022-03-29 19:35:22 +0800

    Update config.js

--- config.js
+++ config.js
@@ -14,8 +14,8 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.linkGC = ['https://chat.whatsapp.com/KG1QdHJ3qkB5QlFW0KeNNJ', 'https://chat.whatsapp.com/KG1QdHJ3qkB5QlFW0KeNNJ ', 'https://chat.whatsapp.com/HBhy7rVae3o0PkIfkhvs2N'] // ganti jadi group lu
-global.owner = ['6285754180729', '6285754180729', '6285754180729'] // Masukan nomot kalian
-global.mods = ['6285754180729', '6285754180729'] // Moderator
+global.owner = ['6282145203493', '6282145203493', '6282145203493'] // Masukan nomot kalian
+global.mods = ['6282145203493', '6282145203493'] // Moderator
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
 global.APIs = { // API Prefix
   // name: 'https://website'
